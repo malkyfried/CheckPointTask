c2a33eeeace3874743871b78e3be4ee6dc2359bd
malkyfried <hand1b22@bjcomp.org>
2024-05-02 23:39:38 +0300

    Update

--- CheckPointTask/Program.cs
+++ CheckPointTask/Program.cs
@@ -1,13 +1,13 @@
 ﻿using CheckPointTask;
 
 string pathToDataFolder = "../../../Data/";
-string fileName = "InputText2.txt";
+string fileName = "InputText1.txt";
 
 // Count amount of each word in specific file
 Dictionary<string, int> wordCount= CountWords.Count(pathToDataFolder + fileName);
 
 // Store the results in S3 bucket
-//await S3Operations.UploadToS3Async(fileName, wordCount);
+await S3Operations.UploadToS3Async(fileName, wordCount);
 
 // Reading results from the S3 bucket
 await S3Operations.DownloadFromS3Async(fileName);
